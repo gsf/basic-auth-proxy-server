c175fc52ebf1d5e494e8b8c32c42ff2648eca0ed
Gabriel Farrell <g@grrawr.com>
2012-10-20 23:07:33 -0400

    Modified package.json for name change

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-//var httpAuth = require('http-auth')
 var httpProxy = require('http-proxy')
 
 var port = process.env.PORT || 9876
+// user:pass pairs must be separated by whitespace
 var pairs = process.env.PAIRS || 'user:pass user2:pass2 user3:pass3'
+var realm = process.env.REALM || 'luxembourg'
+var proxiedHost = process.env.PROXIEDHOST || '127.0.0.1'
+var proxiedPort = process.env.PROXIEDPORT || 8765
 
 // Make an array of the user:pass pairs, base64-encoded for easy comparison
 var encoded = []
-pairs.split(' ').forEach(function (pair) {
+pairs.split(/\s+/).forEach(function (pair) {
   encoded.push(Buffer(pair).toString('base64'))
 })
 
-httpProxy.createServer(basicAuth, 9200, '127.0.0.1').listen(port, function () {
+httpProxy.createServer(basicAuth, proxiedPort, proxiedHost).listen(port, function () {
   console.log('Listening on port ' + port)
 })
 
