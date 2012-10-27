224a5ce1fd415a9be9d88040104f062314dcb378
Gabriel Farrell <g@grrawr.com>
2012-10-27 09:46:29 -0400

    Cleanup

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var hedge = require('hedge')
-var httpProxy = require('http-proxy')
+var proxy = require('http-proxy')
 
-
-var port = process.env.PORT || 9876
-var proxiedHost = process.env.PROXIEDHOST || '127.0.0.1'
-// PROXIEDPORT has to be converted a number. See
-// https://github.com/nodejitsu/node-http-proxy/issues/321
-var proxiedPort = +process.env.PROXIEDPORT || 8765
-var realm = process.env.REALM
 
 // user:pass pairs must be separated by whitespace
 var pairStr = process.env.PAIRS || 'user:pass user2:pass2 user3:pass3'
+var port = process.env.PORT || 9876
+var proxiedHost = process.env.PROXIEDHOST || '127.0.0.1'
+// PROXIEDPORT must be converted to a number. See
+// https://github.com/nodejitsu/node-http-proxy/issues/321
+var proxiedPort = +process.env.PROXIEDPORT || 8765
+var realm = process.env.REALM || 'luxembourg'
+
+// set up validate function to pass to hedge
 var pairs = {}
 pairStr.split(/\s+/).forEach(function (pair) {
   var userPass = pair.split(':')
@@ -21,10 +22,10 @@ function validate (username, password, cb) {
   cb(new Error('No match'))
 }
 
-httpProxy.createServer(
+proxy.createServer(
   hedge({realm: realm, validate: validate}), 
   proxiedPort, 
   proxiedHost
 ).listen(port, function () {
-  console.log('Listening on port ' + port)
+  console.log('Proxying from port '+port+' to '+proxiedHost+':'+proxiedPort)
 })
